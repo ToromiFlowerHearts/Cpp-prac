a4bf1ced369f9c54147508f97beeddabaf8342ee
Toromi <gracecollapce@gmail.com>
2026-09-04 14:51:43 +0500

    Update main.cpp
    Camera control added

--- RaylibCPP/src/main.cpp
+++ RaylibCPP/src/main.cpp
@@ -6,7 +6,6 @@
 #include <optional>
 using namespace std;
 
-
 struct EntityPos
 {
     int x;
@@ -19,106 +18,112 @@ struct EntitySize
     int Width;
 };
 
-enum EntityColor {
-white,
-red,
-blue,
+enum EntityColor
+{
+    white,
+    red,
+    blue,
 };
 
-const Color RaylibColors[] = { WHITE, RED, BLUE, GREEN };
-
+const Color RaylibColors[] = {WHITE, RED, BLUE, GREEN};
 
 struct EntityData
 {
-std::vector<EntityPos> positions;
-std::vector<EntitySize> sizes;
-std::vector<EntityColor> Colors;
+    std::vector<EntityPos> positions;
+    std::vector<EntitySize> sizes;
+    std::vector<EntityColor> Colors;
+    std::vector<std::pair<int, int>> Chunks;
 };
 
-struct ChunkData {
+struct ChunkData
+{
 
-EntityData Chunk [10][10];
-
+    std::vector<EntityPos> positions;
 };
 
-
-void CreateEntity(EntityData & Data, int EntityPosx, int EntityPosy, int Height, int Width, EntityColor color){
-    Data.positions.push_back({EntityPosx,EntityPosy});
-    Data.sizes.push_back({Height,Width});
+void CreateEntity(EntityData &Data, int EntityPosx, int EntityPosy, int Height, int Width, EntityColor color)
+{
+    Data.positions.push_back({EntityPosx, EntityPosy});
+    Data.sizes.push_back({Height, Width});
     Data.Colors.push_back(color);
-}; 
-
-
-
-void DrawEntity(int id, EntityData & Data){
-DrawRectangle(Data.positions[id].x, Data.positions[id].y, Data.sizes[id].Height, Data.sizes[id].Width, RaylibColors[Data.Colors[id]]);
-}
-
-
-void CreateChunk(EntityData & Data, int chunkposX, int chunkposY,int chunkWidth,int chunkHeight, int Height, int Width, EntityColor color ){
-int EntityPosx = chunkposX;
-int EntityPosy = chunkposY; 
-
-
-int counterY = chunkHeight / Height;
-int counterX = chunkWidth / Width;
-
-
-for (int y = 0; y < counterY; y++){
-    for (int x = 0; x < counterX; x++){
-    Data.positions.push_back({EntityPosx,EntityPosy});
-    Data.sizes.push_back({Height,Width});
-    Data.Colors.push_back((EntityColor)GetRandomValue(0, 2));
-    EntityPosx += Width;
-    };
-    EntityPosx = chunkposX;
-    EntityPosy += Height;
-}
 };
-int main(){
 
-EntityData Data;
+void DrawEntity(int id, EntityData &Data)
+{
+    DrawRectangle(Data.positions[id].x, Data.positions[id].y, Data.sizes[id].Height, Data.sizes[id].Width, RaylibColors[Data.Colors[id]]);
+}
 
-int chunkposee = 100; 
-int chunkposeew = 100; 
-for (int i = 0; i < 3; i++){
-    chunkposee += 350;
-    for (int i = 0; i < 3; i++){
-CreateChunk(Data, chunkposee, chunkposeew, 320, 320, 32, 32,(EntityColor)GetRandomValue(0, 2));
-            chunkposee += 350;
+void CreateChunk(EntityData &Data, int chunkposX, int chunkposY, int chunkWidth, int chunkHeight, int Height, int Width, EntityColor color)
+{
 
+    Data.Chunks.push_back({chunkposX, chunkposY});
+    int EntityPosx = chunkposX;
+    int EntityPosy = chunkposY;
+
+    int counterY = chunkHeight / Height;
+    int counterX = chunkWidth / Width;
+
+    for (int y = 0; y < counterY; y++)
+    {
+        for (int x = 0; x < counterX; x++)
+        {
+            Data.positions.push_back({EntityPosx, EntityPosy});
+            Data.sizes.push_back({Height, Width});
+            Data.Colors.push_back((EntityColor)GetRandomValue(0, 2));
+            EntityPosx += Width;
+        };
+        EntityPosx = chunkposX;
+        EntityPosy += Height;
     }
-    chunkposee = 100;
-    chunkposeew += 350;
 };
 
+int main()
+{
+
+    EntityData Data;
+
+    int chunkposee = 100;
+    int chunkposeew = 100;
+    CreateChunk(Data, chunkposee, chunkposeew, 320, 320, 32, 32, (EntityColor)GetRandomValue(0, 2));
+
+    cout << Data.Chunks.size() << "\n";
     InitWindow(1920, 1080, "games");
-    SetTargetFPS(1000000);
+    SetTargetFPS(60);
+
+    Vector2 CameraPos{500, 500};
+    Vector2 CameraOffset{GetScreenHeight() / 2, GetScreenWidth() / 2};
+    Camera2D mainCamera{CameraOffset, CameraPos, 0, 1};
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
-        ClearBackground(BLACK);  
+        ClearBackground(BLACK);
+        BeginMode2D(mainCamera);
+        if (IsKeyDown(KEY_W))
+        {
+            mainCamera.target.y -= 10;
+        };
+        if (IsKeyDown(KEY_S))
+        {
+            mainCamera.target.y += 10;
+        };
+        if (IsKeyDown(KEY_A))
+        {
+            mainCamera.target.x -= 10;
+        };
+        if (IsKeyDown(KEY_D))
+        {
+            mainCamera.target.x += 10;
+        };
 
-
-        for (int i = 0; i <  Data.positions.size(); i++){
-        DrawEntity(i, Data); 
+        for (int i = 0; i < Data.positions.size(); i++)
+        {
+            DrawEntity(i, Data);
         }
+        EndMode2D();
         DrawFPS(10, 10);
         EndDrawing();
-                    };
-        CloseWindow();
-       return 0;
+    };
+    CloseWindow();
+    return 0;
 };
-
-
-
-// for (int i = 1; i < 10000; i++){
-// int x = (GetRandomValue(0, 1000) / 32) * 32;
-// int y = (GetRandomValue(0, 1000) / 32) * 32;
-// CreateEntity(Data, x, y, 32, 32, (EntityColor)GetRandomValue(0, 2)); // ЦИКЛ СОЗДАНИЯ ЭНТИТИ 
-
-// };
-
-
-// СТАРЫЙ ЦИКЛ СОЗДАНИЯ ЭНТИТИ 
